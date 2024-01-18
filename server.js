19561f2842dd75a84f0efbb6693c72285c8e8a06
Jani Nurminen <jani.nurminen2@student.careeria.fi>
2024-01-18 14:35:02 +0200

    Päivityksiä

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Express.js palvelin //
 const path = require('path')
 const express = require('express')
+const fs = require('fs').promises
 
 const app = express()
 
@@ -11,6 +12,24 @@ app.get('/api/herkut', (req, res) => {
     res.json(herkut)
 })
 
+//---------------------------------------------
+
+// Pinkoodin lukeminen txt tiedostosta palvelimelta ja lähettäminen selaimelle
+app.get('/api/getpin', async (req, res) =>{
+    try {
+        // Lue tekstitiedoston tieto
+        const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+
+        // Send the file content as the response
+        res.send(savedPin)
+    } catch (error) {
+        console.error('Error reading file:', error)
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+
+//-------------------------------------------------------
 
 // Tehdään polkumääritys public kansioon
 const polku = path.join(__dirname, './public')
